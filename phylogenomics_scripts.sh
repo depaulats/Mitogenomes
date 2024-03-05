a38d4e6b84348ea57d2a5429c6237f4a1a4a0fc7
Thiago S. de Paula <55711062+depaulats@users.noreply.github.com>
2024-03-04 21:47:45 -0300

    Updated file

--- phylogenomics_scripts.sh
+++ phylogenomics_scripts.sh
@@ -2,24 +2,24 @@
 # If you have the programs in different Conda environments, activate them accordingly
 
 # Move into the working folder
-$ cd /mnt/f/Ubuntu/MT_GB
+cd /mnt/f/Ubuntu/MT_GB
 
 ### If you have individual genbank files already donwloaded...
 
 # Compile a list of all Genbank (gb/gbk) files in the folder
-$ ls *.gb > gb.txt
+ls *.gb > gb.txt
 
 # Create a bash file to use with the pythin script ($ python script.py infile.gb)
-$ sed '/^/ s/^/python gbex_tranlation.py /g' gb.txt > gb00_extract_aa.sh
-$ sed '/^/ s/^/python gbex_rrnaseq.py /g' gb.txt > gb00_extract_rrna.sh
+sed '/^/ s/^/python gbex_tranlation.py /g' gb.txt > gb00_extract_aa.sh
+sed '/^/ s/^/python gbex_rrnaseq.py /g' gb.txt > gb00_extract_rrna.sh
 
 # Extract tranlation sequences from CDS entries in Genbank (GB/GBK) files
-$ bash gb00_extract_aa.sh
-$ bash gb00_extract_rrna.sh
+bash gb00_extract_aa.sh
+bash gb00_extract_rrna.sh
 
 # Concatenate all protein and rRNA sequences in the folder into a single file
-$ cat *.faa > GB_all.faa
-$ cat *.fas > GB_all.fas
+cat *.faa > GB_all.faa
+cat *.fas > GB_all.fas
 
 ### If you don't have the genbank files yet...
 
@@ -28,13 +28,13 @@ $ cat *.fas > GB_all.fas
 # VERY IMPORTANT!! You might need to manually rename the genes for congruence among entries.
 
 # Concatenate your Genbank files with the donwloaded ones into a single file
-$ cat MT08.gb sequence.gb > gb_all.gb
+cat MT08.gb sequence.gb > gb_all.gb
 
 # Extract amino acid sequences from translation features in Genbank (gb/gbk) files (custom Python script provided here)
-$ python gbex_tranlation.py gb_all.gb
+python gbex_tranlation.py gb_all.gb
 
 # Extract DNA sequences from rRNA features in Genbank (gb/gbk) files (custom Python script provided here)
-$ python gbex_rrnaseq.py gb_all.gb
+python gbex_rrnaseq.py gb_all.gb
 
 # Create a folder to hold temporary files (example /genes) but dont move to it (stay in the working folder set above)
 mkdir genes
@@ -81,10 +81,10 @@ mafft --genafpair --maxiterate 1000 genes/gb_rnl.fa > genes/gb_rnl_align.fa
 mafft --genafpair --maxiterate 1000 genes/gb_rns.fa > genes/gb_rns_align.fa
 
 # If you dont have a MAFFT installed...
-$ conda install -c bioconda mafft
+conda install -c bioconda mafft
 
 # If you dont have MAFFT environment activated...
-$ conda activate mafft
+conda activate mafft
 
 # Run Gblocks to remove ambiguiously aligned positions ($ bash gb03_gblocks.sh)
 Gblocks genes/gb_ATP6_align.fa -t=p -b5=a
@@ -105,10 +105,10 @@ Gblocks genes/gb_rnl_align.fa -t=d -b5=a
 Gblocks genes/gb_rns_align.fa -t=d -b5=a
 
 # If you dont have Gblocks installed...
-$ conda install -c bioconda gblocks
+conda install -c bioconda gblocks
 
 # If you dont have a Gblocks environment activated... (in my case, MAFFT and Gblocks are in the same environment)
-$ conda activate gblocks
+conda activate gblocks
 
 # Fix the sequences by removing empty spaces and renaming the headers; headers must be ">ID gene" for concatenation later on ($ bash gb04_fix.sh)
 sed -e 's/ //g; /^>/ s/@/ /g; /^>/ s/_$//g' genes/gb_ATP6_align.fa-gb > genes/gb_ATP6_fix.fa
@@ -148,10 +148,10 @@ seqkit seq genes/gb_rns_fix.fa -w 0 > genes/gb_rns_set.fa
 
 # Concatenate the sequences by IDs
 # If you choose, you can concatenate first the AA and then the DNA sequences to know the range position of each. For such specify the input files one by one (example, 'seqkit concat in1 in2 > out'.
-$ seqkit concat genes/*_set.fa > gb_all_genes.fa
+seqkit concat genes/*_set.fa > gb_all_genes.fa
 
 # The final alignment will be retrieved after renaming the headers, removing the list of genes
-$ sed '/^>/ s/ .*$//g' gb_all_genes.fa > gb_all_accession.fas
+sed '/^>/ s/ .*$//g' gb_all_genes.fa > gb_all_accession.fas
 
 # VERY IMPORTANT! The AA and DNA partitions must be defined manually.
 # Retrieve lenght of the individual genes for partition definition
@@ -162,9 +162,9 @@ for i in genes/*_set.fa-len; do head -n 1 $i >> gb_genes_lenght.txt ; done
 seqkit replace -p "(.+)" -r '{kv}' -k gb_names.txt gb_all_accession.fas > gb_all_final.fas
 
 # Run RAxML-NG to recover the best-scoring ML tree with Bootstrap support
-$ /mnt/f/Ubuntu/raxml-ng/raxml-ng --msa gb_all_final.fas --msa-format FASTA --seed 12345 --model cat_partitions.txt --all --bs-trees autoMRE{500} --bs-metric TBE
+/mnt/f/Ubuntu/raxml-ng/raxml-ng --msa gb_all_final.fas --msa-format FASTA --seed 12345 --model cat_partitions.txt --all --bs-trees autoMRE{500} --bs-metric TBE
 
 # You can remove the temporary files in the 'genes' folder now
-$ rm -rv genes/
+rm -rv genes/
 
 
